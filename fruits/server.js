8a7e345b2d139503b3224e44c4949b3a792f8d40
juleberry <105573843+juleberry@users.noreply.github.com>
2022-09-19 13:18:50 -0400

    Week 9 Changes

--- fruits/server.js
+++ fruits/server.js
@@ -4,12 +4,10 @@
 
 const express = require('express');
 const app = express();
-const fruits = require('./models/fruits');
-const veggies = require('./models/veggies');
 require('dotenv').config();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Fruit = require('./models/Fruit');
-const Veggie = require('./models/Veggie')
+const methodOverride = require('method-override');
 
 // -------------------------
 // Mongoose Connection Stuff
@@ -37,10 +35,17 @@ app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.urlencoded({extended:false}));
 
 // -------------------------
+// Method Override
+// -------------------------
+
+app.use(methodOverride('_method'));
+
+// -------------------------
 // Fruit Routes
 // -------------------------
 
 // Index
+
 app.get('/fruits', (req, res) => {
   Fruit.find({}, (error, allFruits) => {
     res.render('fruits/Index', {
@@ -50,11 +55,46 @@ app.get('/fruits', (req, res) => {
 });
 
 // New
+
 app.get('/fruits/new', (req, res) => {
   res.render('fruits/New');
 });
 
+// Delete
+
+app.delete('/fruits/:id', (req, res) => {
+  Fruit.deleteOne({
+    _id: req.params.id
+  }, (error, data) => {
+    console.log(data);
+    res.redirect('/fruits');
+  })
+});
+
+// Update
+
+app.put('/fruits/:id', (req, res) => {
+  if (req.body.readyToEat === 'on') {
+    req.body.readyToEat = true
+  } else {
+    req.body.readyToEat = false
+  }
+  Fruit.updateOne({
+    _id: req.params.id
+  }, req.body, (error, data) => {
+    if (error) {
+      console.error(error);
+      res.json({
+        error: error
+      });
+    } else {
+      res.redirect(`/fruits/$req.params.id`);
+    }
+  });
+});
+
 // Create
+
 app.post('/fruits', (req, res) => {
   if (req.body.readyToEat === 'on') {
     req.body.readyToEat = true;
@@ -66,7 +106,25 @@ app.post('/fruits', (req, res) => {
   })
 });
 
+// Edit
+
+app.get('/fruits/:id/edit', (req, res) => {
+  Fruit.findOne({
+    _id: req.params.id
+  }, (error, foundFruit) => {
+    if (error) {
+      console.error(error);
+      res.json({
+        error: error
+      })
+    } else {
+      res.render('fruits/Edit', { fruit: foundFruit });
+    }
+  })
+});
+
 // Show
+
 app.get('/fruits/:id', (req, res) => {
   Fruit.findOne({ _id: req.params.id }, (error, foundFruit) => {
     res.render('fruits/Show', {
@@ -80,46 +138,43 @@ app.get('/fruits/:id', (req, res) => {
 // -------------------------
 
 // Index
+
 // Add a Veggie.find to find all of the veggies and pass that to your res.render
+
 app.get('/veggies', (req, res) => {
-  Veggie.find({}, (error, allVeggies) => {
-    res.render('veggies/Index', {
-      veggies: allVeggies
-    });
-  })
+  // res.render('veggies/Index', {
+  //   veggies: veggies
+  // });
 });
 
 // New
+
 // Same as for fruits
+
 app.get('/veggies/new', (req, res) => {
-  res.render('veggies/New');
+  // in here goes your res.redirect to the new FORM
 });
 
 // Create
+
 // Same as for fruits
+
 app.post('/veggies', (req, res) => {
-  if (req.body.readyToEat === 'on') {
-    req.body.readyToEat = true;
-  } else {
-    req.body.readyToEat = false;
-  }
-  Veggie.create(req.body, (error, createdVeggie) => {
-    res.redirect('/veggies')
-  })
-});
+  // In here goes your ready to eat change AND your Veggie.create method
+})
 
 // Show
+
 // Add a Veggie.findOne with the _id of the veggie you want to display. Reference the fruits one!
-app.get('/veggies/:id', (req, res) => {
-  Veggie.findOne({ _id: req.params.id }, (error, foundVeggie) => {
-    res.render('veggies/Show', {
-      veggie: foundVeggie
-    });
-  });
+
+app.get('/veggies/:indexOfArr', (req, res) => {
+  // res.render('veggies/Show', {
+  //   veggie: veggies[req.params.indexOfArr]
+  // });
 });
 
 // -------------------------
-// App Is Listening
+// App Is Listening Thing
 // -------------------------
 
 app.listen(3000, () => {
